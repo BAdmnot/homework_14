5f6d4b0238bd74b80d68388118ca6a75987d97a4
Bogdan Artemenko <bogdan.artvas@gmail.com>
2026-01-05 12:43:12 +0200

    bugfix

--- analyzer/forms.py
+++ analyzer/forms.py
@@ -2,10 +2,4 @@ from django import forms
 
 
 class TextForm(forms.Form):
-    text = forms.CharField(
-        widget=forms.Textarea(attrs={
-            "rows": 150,
-            "cols": 20,
-            "placeholder": "Text"
-        }),
-        required=True)
+    text = forms.CharField(widget=forms.Textarea())
